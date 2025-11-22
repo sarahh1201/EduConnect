810f3ed0f11b78b2f6f96cfb2898c7c3553d75b7
Sarah <shy_h@telus.net>
2025-11-22 15:48:25 -0800

    auth.h changes

--- v2/backend/auth.h
+++ v2/backend/auth.h
@@ -17,6 +17,110 @@ User* verifyUser(vector<User>& users, const string& username, const string& pass
     return nullptr;
 }
 
+void PasswordRecovery(vector<User>& users, const string& user) // Asks the user a security question, and if they get it correct present the current password in the 'server'
+{
+	User* userPtr = nullptr;
+	for (auto& u : users){
+		userPtr = &u;
+		break;
+	}
+	
+	clearScreen(); // Clear the display for this new 'window'
+	cout << "--------------------------------------" << endl;
+	cout << "    Educonnect Password Recovery:" << endl;
+	cout << "--------------------------------------" << endl;
+	cout << "Here is your defined security question:" << endl;
+	string input; // Just the user's input
+    int screwUps = 0; // How long the system will be locked out if you keep mucking it up
+	cin.ignore(); // Stops any unnecessary overflow
+
+	cout << userPtr->securityQ << " ";
+	while (screwUps < 3) // Max of 3 attempts to get this right
+	{
+		cout << "Your Answer: ";
+		getline(cin, input); // Is able to accept a full line as an answer too
+		if (input == userPtr->securityA) // If accepted, show the concurrent password and return
+		{
+			cout << "Answer accepted. Your password is: " << userPtr->password << endl;
+			return;
+		}
+		else // If not accepted, increment the screw up counter
+		{
+			cout << "Incorrect answer." << endl;
+			screwUps++;
+			cout << "Number of attempts remaining: " << (3 - screwUps) << endl;
+		}
+	}
+	cout << "Maximum attempts reached. Please try again later." << endl;	
+	return;		
+}
+
+void PasswordChanger(const vector<User>& users, const string& user) // Does exactly what's on the tin
+{ 
+	User* userPtr = nullptr;
+	for (auto u : users){
+		userPtr = &u;
+		break;
+	}
+	// NOTE: This function is currently unused, be sure to have an option for the user to change their password when logged in!
+	clearScreen(); // Clear way for this cool new window!
+	cout << "--------------------------------" << endl;
+	cout << "   Educonnect Password Changer" << endl;
+	cout << "--------------------------------" << endl;
+	cout << "User ID: "; // First, ask the user to re-input their ID once more for verification
+	string ID;
+	cin >> ID; // Quite aptly
+	cin.clear(); // This is to prevent any other input just randomly being applied to the next time 'cin' is called
+
+	if (ID != userPtr->username) // If the ID doesn't match up, boot them outta here
+	{
+		cout << "Error: User ID does not match logged in user." << endl;
+		return;
+	}
+
+	int screwUps = 0; // Now for the password stage, and for everyone's favourite variable, the screw up counter!
+	string oldPassword; // Let this represent the old password the user will be inputting
+	
+	cout << "Enter your current password: ";
+	cin >> oldPassword;
+
+	while (screwUps < 3) // Max of 3 attempts to get this right
+	{
+		if (oldPassword == userPtr->password) // If accepted, move on to the next stage
+		{
+			string newPassword, confirmPassword; // New password and its confirmation counterpart
+			cout << "Enter your new password: ";
+			cin >> newPassword;
+			cout << "Confirm your new password: ";
+			cin >> confirmPassword;
+
+			if (newPassword == confirmPassword) // If both new passwords match, change it!
+			{
+				userPtr->password = newPassword; // Change the password in the 'server'
+				cout << "Password successfully changed!" << endl;
+				return;
+			}
+			else // If not, inform the user and exit
+			{
+				cout << "Error: New passwords do not match." << endl;
+				return;
+			}
+		}
+		else // If not accepted, increment the screw up counter
+		{
+			cout << "Incorrect current password." << endl;
+			screwUps++;
+			cout << "Number of attempts remaining: " << (3 - screwUps) << endl;
+			if (screwUps < 3) {
+				cout << "Enter your current password: ";
+				cin >> oldPassword;
+			}
+		}
+	}
+	cout << "Maximum attempts reached. Please try again later." << endl;	
+	return;
+}
+
 // ----------------- Login Screen -----------------
 void loginScreen(vector<User>& users) {
     const int maxAttempts = 3;
@@ -31,8 +135,13 @@ void loginScreen(vector<User>& users) {
 
         // Recovery
         if (password =="recover"){
-            passwordRecovery(userID);
+            PasswordRecovery(users,userID);
         }
+		// Change Password
+		if (password =="change"){
+            PasswordChanger(users,userID);
+        }
+		else {continue;}
 
         User* user = verifyUser(users, userID, password);
         if (user) {
@@ -55,104 +164,3 @@ void loginScreen(vector<User>& users) {
     cout << "Maximum login attempts reached. Exiting..." << endl;
 }
 
-void passwordRecovery(const string& user) // Asks the user a security question, and if they get it correct present the current password in the 'server'
-{
-	clearScreen(); // Clear the display for this new 'window'
-	cout << "--------------------------------------" << endl;
-	cout << "    Educonnect Password Recovery:" << endl;
-	cout << "--------------------------------------" << endl;
-	cout << "Here is your defined security question " << user->securityQ << ":" << endl;
-	string input; // Just the user's input
-    int screwUps = 0; // How long the system will be locked out if you keep mucking it up
-	cin.ignore(); // Stops any unnecessary overflow
-	while (true)
-	{
-		cout << question << " ";
-		getline(cin, input); // Is able to accept a full line as an answer too
-		if (input == securityA) // If accepted, show the concurrent password and return
-		{
-			cout << "Answer accepted." << endl;
-			auto item = pass.find(user); // Find the user's password
-			if (item != pass.end()) // If found within the file basically
-			{
-				cout << "Your password is " << item->second << endl; // Second item is their password here
-			}
-			else // If it fails to find the password
-			{
-				cout << "Either no password is set for this user, or the server cannot be reached." << endl;
-			}
-			return;
-		}
-		else
-		{
-			cout << "Incorrect answer." << endl;
-			if (screwUps == 3) // Forces the user to return back to the password screen without any given password
-			{
-				cout << "Max incorrect answers reached. Please try again later" << endl;
-				return;
-			}
-			cout << "Number of attempts remaining: " << (3 - screwUps) << endl;
-		}
-	}
-}
-
-void PasswordChanger(const vector<User>& Users, PassMap& pass, const string& passwordFile = "server.txt") // Does exactly what's on the tin
-{ // NOTE: This function is currently unused, be sure to have an option for the user to change their password when logged in!
-	clearScreen(); // Clear way for this cool new window!
-	cout << "--------------------------------" << endl;
-	cout << "   Educonnect Password Changer" << endl;
-	cout << "--------------------------------" << endl;
-	cout << "User ID: "; // First, ask the user to re-input their ID once more for verification
-	string ID;
-	cin >> ID; // Quite aptly
-	cin.clear(); // This is to prevent any other input just randomly being applied to the next time 'cin' is called
-
-	const User* matchedUser = nullptr; // First, find if this user they inputted does in fact, exist
-	for (const auto& u : Users) if (u.getUserName() == ID) // Should be able to check all users
-	{
-		matchedUser = &u; // When found, we'll be using their credidentials to reset their password
-		break;
-	}
-	else // When no user has been found under this regard
-	{
-		cout << "Unknown User ID. Returning to main menu..." << endl;
-		this_thread::sleep_for(chrono::seconds(2));
-	}
-
-	int screwUps = 0; // Now for the password stage, and for everyone's favourite variable, the screw up counter!
-	string oldPassword; // Let this represent the old password the user will be inputting
-	while (true)
-	{
-		cout << "To verify; please type in the old password prior: ";
-		cin >> oldPassword;
-		if (verify_password(pass, ID, oldPassword)) // Runs a true/false check. When true, the user can input their new password here
-		{
-			cout << "Password accepted." << endl;
-			cout << "Now please enter the new password you wish to overwrite the prior one: ";
-			string newPassword; // And now let the user write their new password
-			cin >> newPassword;
-			cin.clear();
-			if (set_password(pass, ID, newPassword, passwordFile)) // Do effectively another true/false check on the password being written
-			{
-				cout << "Password has been saved to the company server." << endl;
-			}
-			else // If it fails for whatever reason
-			{
-				cout << "Error: Unable to write to the credentials file." << endl;
-			}
-		}
-		else // If the user messes up putting their password in
-		{
-			cout << "Incorrect password." << endl;
-			screwUps++; // User only gets 3 chances before they're booted outta here
-			cout << "Number of attempts remaining: " << (3 - screwUps) << endl;
-			if (screwUps == 3) // Once the threshold is hit, kick them back to the menu
-			{
-				cout << "Returning to the main menu..." << endl;
-				this_thread::sleep_for(chrono::seconds(3));
-				return;
-			}
-			this_thread::sleep_for(chrono::seconds(3));
-		}
-	}
-}
