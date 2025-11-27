5021b85e0b41e2f878e9b944d498423ee72c738a
Sarah <154954522+sarahh1201@users.noreply.github.com>
2025-11-27 08:42:15 -0800

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_EduConnect_FILE /Users/sarahhill/Documents/Workspaces/Algorithms & Data Structures/EduConnect/build/EduConnect.app/Contents/MacOS/EduConnect)
-set(__QT_DEPLOY_TARGET_EduConnect_TYPE EXECUTABLE)
