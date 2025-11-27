5fd8ba90ffa680adb5961545ade9c5f4bb46fa54
Sarah <shy_h@telus.net>
2025-11-27 08:36:48 -0800

    Remove git ignore because I can't see the files for v_ui

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_EduConnect_FILE /Users/sarahhill/Documents/Workspaces/Algorithms & Data Structures/EduConnect/build/EduConnect.app/Contents/MacOS/EduConnect)
+set(__QT_DEPLOY_TARGET_EduConnect_TYPE EXECUTABLE)
